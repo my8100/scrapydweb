cf406035ddc9d9276e410f8b4cd755407c5deb80
guishake <guillaume@retailshake.com>
2021-10-20 21:37:21 +0200

    ci: add option py-executable as per https://uwsgi-docs.readthedocs.io/en/latest/Changelog-2.0.20.html
    enables to override sys.executable used by the app

--- launcher.sh
+++ launcher.sh
@@ -2,4 +2,9 @@
 
 echo "Launching scapydweb"
 
-uwsgi -H /usr/local --http 0.0.0.0:5000 --enable-threads --chdir /app/scrapyd_server --module "scrapydweb.run:main()" 
+uwsgi --py-executable /usr/local/bin/python \
+      --http 0.0.0.0:5000 \
+      --enable-threads \
+      --chdir /app/scrapyd_server \
+      --module "scrapydweb.run:main()" 
+
