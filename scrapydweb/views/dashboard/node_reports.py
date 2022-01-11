b86ce69270724ee2428e78dec9f4af1f81d13e27
h4r1c0t <sebastien@retailshake.com>
2022-01-11 15:56:13 +0100

    :ambulance: (node_reports.py) hotfix on node_reports page
    try to momentarily desactivate the alert_threshold feature to fix the 500 error on scrapydweb node reports page
    
    Closes #1279

--- scrapydweb/views/dashboard/node_reports.py
+++ scrapydweb/views/dashboard/node_reports.py
@@ -67,13 +67,14 @@ class NodeReportsView(BaseView):
                     df = mtm.compute_floating_deviation(df, "items", 7)
                     df = mtm.compute_floating_deviation(df, "pages", 7)
 
-                    items_alert_lvl = mtm.set_alert_level(df, 'items')        
-                    pages_alert_lvl = mtm.set_alert_level(df, 'pages')
-                    
-                    job['alert_indicator'] = mtm.check_alert_level(
-                        [items_alert_lvl, pages_alert_lvl],
-                        2
-                    )
+                # DEBUG >>> 
+                    job['alert_level'] = 0
+                    job['alert_indicator'] = ""
+
+                #     items_alert_lvl = mtm.set_alert_level(df, 'items')        
+                #     job['alert_level'] = items_alert_lvl
+
+                #     job['alert_indicator'] = mtm.check_alert_level([items_alert_lvl])
             
                     self.finished_jobs.append(job)
                 else:
