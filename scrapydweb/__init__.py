6814edb8da926d6c4871e0dd450dfc1d24f5a7b4
nkovacic99 <nace.kovacic007@gmail.com>
2023-10-19 12:05:25 +0200

    no need for a /hello path on the site

--- scrapydweb/__init__.py
+++ scrapydweb/__init__.py
@@ -76,10 +76,6 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
     handle_db(app)
     handle_route(app)
     handle_template_context(app)
