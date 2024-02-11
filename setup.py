45d436e23213ed367306191e8e00750e2c88caa7
LxL <my8100@gmail.com>
2024-02-11 20:01:51 +0800

    Update dependencies and support Python 3.6 to 3.12

--- setup.py
+++ setup.py
@@ -31,29 +31,29 @@ setup(
     packages=find_packages(exclude=("tests", )),
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <=3.9",
+    python_requires=">=3.6",
     install_requires=[
-        "APScheduler==3.5.3",  # Aug 15, 2018
-        "click==7.0",  # Sep 26, 2018
+        "APScheduler==3.6.0",  # Mar 18, 2019
+        "click==7.1.2",  # Apr 28, 2020
         "colorama==0.4.0",  # Oct 10, 2018
-        "Flask==1.0.2",  # May 2, 2018
+        "Flask==2.0.0",  # May 12, 2021
         "Flask-Compress==1.4.0",  # Jan 5, 2017
         "Flask-SQLAlchemy==2.4.0",  # Apr 25, 2019
-        "idna==2.7",  # Jun 11, 2018    
-        "itsdangerous==1.1.0",  # Oct 27, 2018
-        "Jinja2==2.10",  # Nov 9, 2017
+        "idna==2.7",  # Jun 11, 2018
+        "itsdangerous==2.0.0",  # May 12, 2021
+        "Jinja2==3.0.0",  # May 12, 2021
         "logparser==0.8.2",
-        "MarkupSafe==1.1.1",  # Feb 24, 2019
+        "MarkupSafe==2.0.0",  # May 12, 2021
         "pexpect==4.7.0",  # Apr 7, 2019
         "ptyprocess==0.6.0",  # Jun 22, 2018
         "pytz==2018.9",  # Jan 7, 2019
         "requests>=2.21.0",  # Dec 10, 2018
         "setuptools>=40.6.3",  # Dec 11, 2018
-        "six==1.12.0",  # Dec 10, 2018
+        "six==1.16.0",  # May 5, 2021
         "SQLAlchemy==1.3.24",  # Mar 31, 2021
         "tzlocal==1.5.1",  # Dec 1, 2017
-        "w3lib==1.19.0",  # Jan 25, 2018
-        "Werkzeug==0.14.1",  # Jan 1, 2018
+        "w3lib==2.0.0",  # Aug 11, 2022
+        "Werkzeug==2.0.0",  # May 12, 2021
     ],
 
     entry_points={
@@ -64,10 +64,12 @@ setup(
 
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ]
 )
