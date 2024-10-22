09c5b6119377ac3bf01b8f74bf836959b99b3982
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2024-10-22 10:36:32 -0400

    Revert Updates

--- setup.py
+++ setup.py
@@ -32,13 +32,26 @@ setup(
     python_requires=">=3.6",
     install_requires=[
         "APScheduler==3.6.0",  # Mar 18, 2019
+        "click==7.1.2",  # Apr 28, 2020
         "colorama==0.4.0",  # Oct 10, 2018
-        "Flask==2.2.5",  # May 12, 2021
+        "Flask==2.0.0",  # May 12, 2021
         "Flask-Compress==1.4.0",  # Jan 5, 2017
-        "Flask-SQLAlchemy>=3.1.1",  # Apr 25, 2019
+        "Flask-SQLAlchemy==2.4.0",  # Apr 25, 2019
+        "idna==2.7",  # Jun 11, 2018
+        "itsdangerous==2.0.0",  # May 12, 2021
+        "Jinja2==3.0.0",  # May 12, 2021
         "logparser==0.8.2",
+        "MarkupSafe==2.0.0",  # May 12, 2021
+        "pexpect==4.7.0",  # Apr 7, 2019
+        "ptyprocess==0.6.0",  # Jun 22, 2018
+        "pytz==2018.9",  # Jan 7, 2019
         "requests>=2.21.0",  # Dec 10, 2018
+        "setuptools>=40.6.3",  # Dec 11, 2018
+        "six==1.16.0",  # May 5, 2021
+        "SQLAlchemy==1.3.24",  # Mar 31, 2021
+        "tzlocal==1.5.1",  # Dec 1, 2017
         "w3lib==2.0.0",  # Aug 11, 2022
+        "Werkzeug==2.0.0",  # May 12, 2021
     ],
     entry_points={"console_scripts": {"scrapydweb = scrapydweb.run:main"}},
     classifiers=[
